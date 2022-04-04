d8ce7588248ea4f1c0d733d2cda86fa8210b8f1b
vegardjervell <vegard.g.j@icloud.com>
2022-04-04 16:05:43 +0200

    Modified build script to run tests before pushing to pypi

--- build_dist.sh
+++ build_dist.sh
@@ -2,5 +2,13 @@
 # TODO: Add code that bumps version number
 
 bash cpp/build_mac.sh
+python -m pykingas -test
+status=$?
+if [[ $status -ne 0 ]]; then
+    echo "pykingas-tests failed with exit code $status"
+    exit $status
+else
+    echo "pykingas-tests were successful"
+fi
 python setup.py sdist bdist_wheel
 twine upload -r pypi dist/*
