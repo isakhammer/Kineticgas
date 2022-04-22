1ac152d1b9b7620d451ba5a02c95e3426296935a
vegardjervell <vegard.g.j@icloud.com>
2022-04-22 15:03:17 +0200

    Added line to run python tests after building

--- cpp/build_mac.sh
+++ cpp/build_mac.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 cd cpp/release
+rm -rf CMakeCache.txt
 cmake -DCMAKE_BUILD_TYPE=Release  ..
 make
 cd ../..
 
 cp cpp/release/KineticGas.cpython-39-darwin.so pykingas/KineticGas.so
+python pykingas/test_inside.py
