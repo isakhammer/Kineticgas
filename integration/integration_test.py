d893e27640e22fd0268c0e4df331b0c58b6b9b65
vegardjervell <vegard.g.j@icloud.com>
2022-04-29 02:00:29 +0200

    Wrote test for integration scheme, shit works yo

--- integration/integration_test.py
+++ integration/integration_test.py
@@ -8,6 +8,8 @@ if '-debug' in sys.argv or '-Debug' in sys.argv or '-d' in sys.argv:
 else:
     from integration import Integration_r as I
 
+FLTEPS = 1e-12
+
 def test_line():
     x = [1, 2]
     y = [2, 3]
@@ -47,43 +49,80 @@ def test_plane():
     ax.set_ylabel('y')
     plt.show()
 
+def plot_plane(p1, p2, p3):
+    x = [p1.x, p2.x, p3.x]
+    y = [p1.x, p2.y, p3.y]
+    z = [p1.z, p2.z, p3.z]
+
+    plane = I.get_plane(p1, p2, p3)
+
+    def f(x_val, y_val):
+        val = plane.A * x_val + plane.B * y_val + plane.C
+        return val
+
+    l12 = I.get_line(p1, p2)
+    l13 = I.get_line(p1, p3)
+    l23 = I.get_line(p2, p3)
+
+    y_func = lambda x_val, l: l.a * x_val + l.b
+
+    x_list = np.linspace(min(x), max(x), 10)
+    y_list = np.linspace(min(y), max(y), 10)
+    x_ax, y_ax = np.meshgrid(x_list, y_list)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z, color='r')
+
+    ax.plot_wireframe(x_ax, y_ax, f(x_ax, y_ax), color='black')
+    ax.plot(x_list, y_func(x_list, l13), f(x_list, y_func(x_list, l13)), color='r', label='l13')
+    ax.plot(x_list, y_func(x_list, l12), f(x_list, y_func(x_list, l12)), color='g', label='l12')
+    ax.plot(x_list, y_func(x_list, l23), f(x_list, y_func(x_list, l23)), color='b', label='l23')
+    ax.set_ylim(min(y), max(y))
+    ax.set_xlim(min(x), max(x))
+    ax.set_xlim(min(z), max(z))
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    plt.show()
+
 def test_integrate(do_plot=False):
-    x = [0, 1, 1]
+    x = [0, 1, 2]
     y = [0, 2, 0]
     z = [0, 3, 0]
     p1 = I.Point(x[0], y[0], z[0])
     p2 = I.Point(x[1], y[1], z[1])
     p3 = I.Point(x[2], y[2], z[2])
 
-    plane = I.get_plane(p1, p2, p3)
     integ = I.integrate(p1, p2, p3) # V = (1 / 3) * A * h for en pyramide
-    print(integ)
-    if do_plot:
-        def f(x_val, y_val):
-            val = plane.A * x_val + plane.B * y_val + plane.C
-            return val
+    if abs(integ - 2) > FLTEPS:
+        if do_plot:
+            print(1, integ - 2)
+            plot_plane(p1, p2, p3)
+        return 1
 
-        l12 = I.get_line(p1, p2)
-        l13 = I.get_line(p1, p3)
-        l23 = I.get_line(p2, p3)
-        print(l12.a, l13.a, l23.a)
-        y_func = lambda x_val, l: l.a * x_val + l.b
+    x = [0, 1, 1]
+    p1 = I.Point(x[0], y[0], z[0])
+    p2 = I.Point(x[1], y[1], z[1])
+    p3 = I.Point(x[2], y[2], z[2])
+    integ = I.integrate(p1, p2, p3)
+    if abs(integ - 1) > FLTEPS:
+        if do_plot:
+            print(2, integ - 1)
+            plot_plane(p1, p2, p3)
+        return 2
 
-        x_list = np.linspace(0, 2, 10)
-        y_list = np.linspace(0, 2, 10)
-        x_ax, y_ax = np.meshgrid(x_list, y_list)
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(x, y, z, color='r')
+    x = [1, 1, 2]
+    p1 = I.Point(x[0], y[0], z[0])
+    p2 = I.Point(x[1], y[1], z[1])
+    p3 = I.Point(x[2], y[2], z[2])
+    integ = I.integrate(p1, p2, p3)
+    if abs(integ - 1) > FLTEPS:
+        if do_plot:
+            print(3, integ - 1)
+            plot_plane(p1, p2, p3)
+        return 3
 
-        ax.plot_wireframe(x_ax, y_ax, f(x_ax, y_ax), color='black')
-        ax.plot(x_list, y_func(x_list, l13), f(x_list, y_func(x_list, l13)), color='r', label='lower')
-        ax.plot(x_list, y_func(x_list, l12), f(x_list, y_func(x_list, l12)), color='g', label='upper')
-        ax.plot(x_list, y_func(x_list, l23), f(x_list, y_func(x_list, l23)), color='b', label='upper')
-        ax.set_ylim(0, 2)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        plt.show()
+    return 0, 0
+
 
 
 #test_plane()
