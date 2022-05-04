1d597c7daa26b64bb6ae9d5acb5babe0afbf6e52
vegardjervell <vegard.g.j@icloud.com>
2022-05-04 21:55:26 +0200

    Testing params for better integration

--- integration/kin_integral_tests.py
+++ integration/kin_integral_tests.py
@@ -16,30 +16,77 @@ if '-debug' in sys.argv or '-Debug' in sys.argv or '-d' in sys.argv:
 else:
     from integration import Integration_r as I
 
-kin = KineticGas('AR,C1', potential='mie')
+kin = KineticGas('AR,C1', potential='hs')
 sigma = kin.sigma_ij[0, 0]
 T = 300
 chi_func = lambda g, b: kin.cpp_kingas.chi(1, T, g, b)
 
 def plot_triangluation(r=1, l=1):
     # Using b = b / sigma
-    neval = 0
-    def kinfunc(ij, T, g, b, l, r):
-        nonlocal neval
-        neval += 1
-        return 2 * exp(-g**2) * g**(2 * r + 3) * (1 - (cos(chi_func(g, b * sigma)) ** l)) * b
-    originxy = (1e-5, 1e-5)
+    N_EVAL = 0
+    T_EVAL = 0
+    def w_integrand(ij, T, g, b, l, r):
+        nonlocal N_EVAL, T_EVAL
+        N_EVAL += 1
+        te0 = time.process_time()
+        val = 2 * exp(-g**2) * g**(2 * r + 3) * (1 - (cos(kin.cpp_kingas.chi(1, T, g, b * sigma)) ** l)) * b
+        te1 = time.process_time()
+        T_EVAL += te1 - te0
+        return val
+    originxy = (1e-7, 1e-7)
     endxy = (7.5, 5)
     origin = I.Point(originxy[0], originxy[1])
     end = I.Point(endxy[0], endxy[1])
-    dx, dy = 0.1, 0.1
-    refinement_levels = 8
-    subdomain_dblder_limit = 0.05
-    verts = I.trisurf(origin, end, dx, dy, refinement_levels, subdomain_dblder_limit, 1, 300, l, r, kinfunc)
-    neval = 0
+    dx, dy = 0.45, 0.05
+    refinement_levels_x = 4
+    refinement_levels_y = 8
+    subdomain_dblder_limit = 0.1
+
+    N_EVAL = 0
+    T_EVAL = 0
+    t0 = time.process_time()
+    numeric = I.integrate2d(origin, end, dx, dy, refinement_levels_x, refinement_levels_y, subdomain_dblder_limit, 1, 300, l, r, w_integrand)
+    t1 = time.process_time()
+    t_tot = t1 - t0
+    n_evauated = N_EVAL
+    t_evaluated = T_EVAL
+
+    analytic = kin.cpp_kingas.w_HS(1, T, l, r)
+    rel_error = 100 * ((numeric / analytic) - 1)
+
+
+    verts = I.trisurf(origin, end, dx, dy, refinement_levels_x, refinement_levels_y, subdomain_dblder_limit, 1, 300, l, r, w_integrand)
     verts = np.array(verts)
     x, y, z = verts.reshape((verts.shape[0] * verts.shape[1], verts.shape[2])).transpose()
-    print('Generated :', verts.shape[0], 'triangles from ', x.shape, 'points')
+    n_integrals = verts.shape[0]
+    npoints = x.shape[0]
+
+    out_lines = []
+    out_lines.append('l = ' + str(l) + ', r = ' + str(r))
+    out_lines.append('Origin : ' + str(originxy) + ', End : ' + str(endxy))
+    out_lines.append('dg = ' + str(dx) + ', db = ' + str(dy))
+    out_lines.append('Refinement g, b : ' + str(refinement_levels_x) + ', ' + str(refinement_levels_y) + ', limit : ' + str(subdomain_dblder_limit))
+    out_lines.append('')
+    out_lines.append('Analytic integral : ' + str(round(analytic, 1)))
+    out_lines.append('Numeric integral : ' + str(round(numeric, 1)))
+    out_lines.append('Error : ' + str(round(rel_error, 2)) + ' %')
+    out_lines.append('')
+    out_lines.append('Computation time : ' + str(round(t_tot, 4)) + ' s')
+    out_lines.append('Evaluations : ' + str(n_evauated))
+    out_lines.append('Total points : ' + str(npoints))
+    out_lines.append('Time per evaluation : ' + str(round(1e3 * t_evaluated / n_evauated, 4)) + ' ms')
+    out_lines.append('Time per sub-integral : '+str(round(1e3 * t_tot / n_integrals, 4)) + ' ms')
+    out_lines.append('Time per pre-computed point : ' + str(round(1e3 * (t_tot - t_evaluated) / (npoints - n_evauated), 4)) + ' ms')
+
+    with open('refine_params.txt', 'a') as file:
+        for line in out_lines:
+            print(line)
+            file.write(line + '\n')
+
+        file.write('\n')
+        file.write('#' * 50)
+        file.write('\n\n')
+
     cmap = get_cmap('Blues')
     colors = [cmap(i / 3) for i in range(1, 4)]
     cc = cycler(color=colors)
@@ -96,7 +143,7 @@ def compare_to_HS():
 #compare_to_HS()
 
 
-plot_triangluation(r=1, l=6)
+plot_triangluation(r=5, l=9)
 exit(0)
 #xs = [1, 1, 3]
 #ys = [1, 2, 1]
