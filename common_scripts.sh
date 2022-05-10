2add08bb270286b6e4a91b5957e8ac514d564d6a
isakhammer <isakhammer@gmail.com>
2022-05-10 10:53:20 +0200

    Added build script

--- common_scripts.sh
+++ common_scripts.sh
@@ -1,2 +1,12 @@
 alias python="python3.9"
-alias hal="echo ' hallo ' "
+alias exit_container="exit"
+alias so="source ~/.bashrc"
+
+function build(){
+    cd /root/code
+    rm -Rf cpp/release/*
+    cd cpp/release && cmake -DCMAKE_BUILD_TYPE=Release  .. && make
+    cp cpp/release/KineticGas*.so pykingas/KineticGas.so
+}
+
+alias b="build"
